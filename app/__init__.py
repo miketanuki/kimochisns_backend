cf2c57383dd397584c5241c011ba294d06e47ab9
miketanuki <idyuuki999@gmail.com>
2023-07-08 14:54:21 +0900

    update:dev用ときりわけ

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['ENV'] = ''
     CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    if app.config['ENV'] == 'production':
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'app.db')}"
     db.init_app(app)
     Migrate(app, db)
 
