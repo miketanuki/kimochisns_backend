fdc803c8955e8682a4046d845419d5413adf0fbb
miketanuki <idyuuki999@gmail.com>
2023-12-03 23:12:18 +0900

    環境変数の設定修正とOpenAIのアップデート

--- app/nlp.py
+++ app/nlp.py
@@ -1,16 +1,15 @@
 import os
-# from dotenv import load_dotenv
-import openai
+from dotenv import load_dotenv
+from openai import OpenAI
 
 # APIキーの設定
-# load_dotenv()
-# openai.api_key = os.environ["OPENAI_API_KEY"]
-openai.api_key = os.getenv('OPENAI_API_KEY')
+load_dotenv()
+client = OpenAI()
 
 def analyze_emotion(text):
 
     # OpenAI APIを使用して感情分析を実行する
-    response = openai.ChatCompletion.create(
+    response =  client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
         {"role": "system", "content": "あなたは感情分析サービスです。userから送信された文章の感情がポジティブなら「9」ネガティブなら「0」どちらでもないなら「4」を基準としスコアリングしてください"},
@@ -22,6 +21,6 @@ def analyze_emotion(text):
 
 
     # APIのレスポンスから感情分析結果を取得する
-    result = response["choices"][0]["message"]["content"]
-    print(result)
+    print(response)
+    result = response.choices[0].message.content
     return result
